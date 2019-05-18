b8679b676476ea3e2bf3363f2a7b50a77877c880
lijianxin <lijianxin@bytedance.com>
2019-05-18 17:46:15 +0800

    [enhance] tinypng action dissmiss when dir or non-image file selected

--- src/main/java/TinyPNGAction.java
+++ src/main/java/TinyPNGAction.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.vfs.VirtualFile;
 import download.DownloadManager;
 import download.FileTask;
+import org.jetbrains.annotations.NotNull;
 import upload.UploadManager;
 
 import java.io.File;
@@ -19,5 +20,24 @@ public class TinyPNGAction extends AnAction {
         UploadManager uploadManager = new UploadManager();
         String url = uploadManager.uploadImage(new File(selectedFiles[0].getPath()));
         DownloadManager.getInstance().downloadFile(new FileTask(url, selectedFiles[0].getPath()));
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        super.update(e);
+        VirtualFile[] selectedFiles = DataKeys.VIRTUAL_FILE_ARRAY.getData(e.getDataContext());
+        if (selectedFiles == null || selectedFiles.length == 0) {
+            return;
+        }
+        for (VirtualFile file : selectedFiles) {
+            if (file.isDirectory() || !isImage(file)) {
+                e.getPresentation().setEnabledAndVisible(false);
+                return;
+            }
+        }
+    }
+
+    private boolean isImage(VirtualFile file) {
+        return file != null && file.getExtension() != null && (file.getExtension().equals("png") || file.getExtension().equals("jpeg") || file.getExtension().equals("jpg"));
     }
 }
