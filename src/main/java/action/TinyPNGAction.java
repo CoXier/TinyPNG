74b7f450653293db032e6a822ee8efe2e13bad03
lijianxin <lijianxin@bytedance.com>
2019-05-22 15:48:19 +0800

    [feature] prepare deploy

--- src/main/java/action/TinyPNGAction.java
+++ src/main/java/action/TinyPNGAction.java
@@ -1,3 +1,5 @@
+package action;
+
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
--- src/main/java/api/NetworkUtils.java
+++ src/main/java/api/NetworkUtils.java
@@ -2,6 +2,12 @@ package api;
 
 import okhttp3.OkHttpClient;
 
+import java.util.concurrent.TimeUnit;
+
 public class NetworkUtils {
-    public static OkHttpClient client = new OkHttpClient();
+    public static OkHttpClient client = new OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build();
 }
